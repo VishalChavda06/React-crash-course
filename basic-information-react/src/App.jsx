4ebcc720f7b27a41bf016681362a5a340f961642
VishalChavda06 <vc229067@gmail.com>
2025-12-02 11:16:56 +0530

    first commit

---
+++ basic-information-react/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Header from './Header'
+
+function App() {
+
+  return (
+    <>
+      <Header/>
+      <h1>Hello I'm Vishal Chavda</h1>
+      <p>I'm a web developer</p>
+    </>
+  )
+}
+
+export default App
