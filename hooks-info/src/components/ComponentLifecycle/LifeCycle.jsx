a1d40e115f4c059103dc58885ac6a0c423c295af
VishalChavda06 <vc229067@gmail.com>
2025-12-08 15:36:42 +0530

    Life Cycle Info.

--- hooks-info/src/components/ComponentLifecycle/LifeCycle.jsx
+++ hooks-info/src/components/ComponentLifecycle/LifeCycle.jsx
@@ -1,7 +1,35 @@
-const LifeCycle=()=>{
-    return(
+import { use, useEffect, useState } from "react";
+
+const LifeCycle = () => {
+
+    const [count, setCount] = useState(0)
+    const [display , setDisplay]= useState(true)
+
+    // Mounting
+    useEffect(() => {
+        console.log("Mounting Component Called")
+    }, [])
+
+    //  Updating 
+    useEffect(() => {
+        console.log("Updating Compenent Called")
+    }, [count])
+
+    // Unmounting
+    useEffect(() => {
+        return () => {
+            console.log("Unmounting Component Called")
+        }
+    }, [])
+    return (
         <>
-        <h1 className="text-2xl font-bold mt-10">Life Cycle</h1>
+            {display && <h1 className="text-2xl font-bold mt-10">
+                <h1>Life Cycle</h1>
+                Count is : {count}</h1>}
+            <button className="bg-red-500 text-white p-2 rounded-md" onClick={() => setDisplay(!display)}>Toggle Display</button>
+            {display && <button className="bg-blue-500 text-white p-2 rounded-md" onClick={() => setCount(count + 1)}>Increment ✚</button>}
+            
+
         </>
     )
 }
